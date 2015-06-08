0fc60f34e8c169dd3da971f8203804d5b519f43a
y2q-actionman <y2q-actionman@users.noreply.github.com>
2015-06-08 13:47:35 +0900

    added integer test cases

--- test/util.lisp
+++ test/util.lisp
@@ -1,6 +1,3 @@
 (in-package :jp-numeral.test)
 
-(defmacro assert-progn (&body forms)
-  `(progn ,@(loop for i in forms
-	       collect `(assert ,i))
-	  t))
+(define-modify-macro mulf (&rest args) *)
