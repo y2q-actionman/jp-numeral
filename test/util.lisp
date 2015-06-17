be0d3ae0248b030fd37c7a58ed8e95607ca4dae9
y2q-actionman <y2q-actionman@users.noreply.github.com>
2015-06-17 12:44:47 +0900

    changed tests

--- test/util.lisp
+++ test/util.lisp
@@ -9,10 +9,29 @@
     (:old (values nil t))
     (:normal (values nil nil))))
 
-(defun jpn (num style
-	    &key digits-after-dot scale radix-point)
-  (multiple-value-bind (colon-p at-sign-p)
-      (style-to-flag style)
-    (with-output-to-string (stream)
-      (jp-numeral:jp stream num colon-p at-sign-p
-		     digits-after-dot scale radix-point))))
+(defmacro define-jp-stringify (name lambda-list function)
+  (let ((style-sym (first lambda-list))
+	(num-sym (second lambda-list))
+	(keyargs (loop for i on lambda-list
+		    when (eq (car i) '&key)
+		    return (cdr i)
+		    finally (return nil))))
+    `(defun ,name (,style-sym ,num-sym &key ,@keyargs)
+       (multiple-value-bind (colon-p at-sign-p)
+	   (style-to-flag ,style-sym)
+	 (with-output-to-string (stream)
+	   (,function stream ,num-sym colon-p at-sign-p
+		      ,@keyargs))))))
+
+(define-jp-stringify jp-str
+    (style num &key digits-after-dot scale radix-point)
+  jp-numeral:jp)
+
+(define-jp-stringify wari-str
+    (style num &key digits-after-dot)
+  jp-numeral:wari)
+
+(defun equal-or-free-format (style expect number)
+  (let ((actual-string (jpn style number)))
+    (or (equal expect actual-string)
+	(format nil "~F" number))))
